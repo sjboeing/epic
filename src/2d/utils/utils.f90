ce9da2a56bbb7237f73f7b2236a6a79fac95b14b
willjones041 <mn19wej@leeds.ac.uk>
2025-11-10 09:59:59 +0000

    more fixes for implementing evaporation, utils

--- src/2d/utils/utils.f90
+++ src/2d/utils/utils.f90
@@ -112,7 +112,7 @@ module utils
             call grid2par(parcels%delta_pos, parcels%delta_vor, parcels%strain)
 
             if(microphysics%l_precipitation) then
-                call prec_grid2par(parcels%delta_pos)
+                call prec_grid2par(parcels%delta_pos,parcels%theta,parcels%qv)
             endif
 
             call calculate_parcel_diagnostics(parcels%delta_pos)
